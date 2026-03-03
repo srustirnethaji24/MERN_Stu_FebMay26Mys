ccde291e29e8ac25496b2820f3a8a16c4c50ce75
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-03 15:42:07 +0530

    Arithmetic Operators

---
+++ w3/D2/P1.js
@@ -0,0 +1,14 @@
+//Arithmetic Operators
+console.log("Arithmetic Operators");
+const a=2,b=5;
+let c=0;
+console.log("a+b:", a+b);
+console.log("a-b:", a-b);
+console.log("a*b:", a*b);
+console.log("a/b:", a/b);
+console.log("a%b:", a%b);//Modules
+console.log("a**b:", a**b);//Exponential
+console.log("a++:", a++); //post-increment
+console.log("++a:", ++a);// pre-increment
+console.log("a--:", a--); //post-decrement
+console.log("--a:", --a); //pre-decrement
