87148a42189d632088296bb65fa6e1fb634634f8
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-03 15:45:31 +0530

    Comparison operators

---
+++ w3/D2/P2.js
@@ -0,0 +1,11 @@
+//Comparison operators
+console.log("Comparison operators")
+let x=2,y='4';
+console.log("x==y:",x==y);   //Loose equality
+console.log("x===y:",x===y); //Strict equality
+console.log("x!=y:",x!=y);   //Loose inequality
+console.log("x!==y:",x!==y); //Strict inequality
+console.log("x>3:",x>3);
+console.log("x<3:",x<3);
+console.log("x>=3:",x>=3);
+console.log("x<=3:",x<=3);
