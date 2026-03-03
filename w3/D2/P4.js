76f5005d483b8ae8be80edf6603a07efad2f2a2a
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-03 22:02:21 +0530

    Logical operators

--- w3/D2/P4.js
+++ w3/D2/P4.js
@@ -1,32 +1,32 @@
 // // Conditional statements
-// let age =21;
-// if(age<13){
-//    console.log("Child");
-// }
-// else if (age<18){
-//       console.log("Teenager");
-// }
-// else
-// {
-//     console.log("Adult");
-// }
-// //switch statements
-// console.log("Switch statements");
-// let day = "Tuesday";
-// switch(day){
-//     case"Monday":
-//     console.log("start of the week");
-//     break;
-//     case"Wednesday":
-//     console.log("Mid of the week");
-//     break;
-//     case"Friday":
-//     console.log("End of work week");
-//     break;
-//     default:
-//         console.log("Some day in the week");
-//         break;
-// }
+let age =21;
+if(age<13){
+   console.log("Child");
+}
+else if (age<18){
+      console.log("Teenager");
+}
+else
+{
+    console.log("Adult");
+}
+//switch statements
+console.log("Switch statements");
+let day = "Tuesday";
+switch(day){
+    case"Monday":
+    console.log("start of the week");
+    break;
+    case"Wednesday":
+    console.log("Mid of the week");
+    break;
+    case"Friday":
+    console.log("End of work week");
+    break;
+    default:
+        console.log("Some day in the week");
+        break;
+}
 // Type conversion
 let n = Number("123");
 console.log("n:",n,"Type of n:", typeof n, "isNaN",isNaN(n));
