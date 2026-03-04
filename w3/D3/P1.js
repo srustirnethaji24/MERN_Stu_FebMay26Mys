ae195959145295a077ff1f6d4dacd5be8a455587
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-04 12:19:08 +0530

    For loop

---
+++ w3/D3/P1.js
@@ -0,0 +1,15 @@
+// For loop
+console.log("for loop examples ");
+
+for(let i=0;i<5;i++){ //forward loop
+    console.log("i",i);
+}
+for(let i=6;i>0;i--){ //backward loop
+    console.log("i",i);
+}
+// Nested for loop
+for(let i=1;i<=3;i++){
+    for(let j=1;j<=2;j++){
+        console.log(`i=${i},j=${j}`);
+    }
+}
