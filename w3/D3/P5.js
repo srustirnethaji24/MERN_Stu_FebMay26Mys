c8ad58cb8fffeca32cd8d9e52238d2428ffab3b3
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-04 12:39:35 +0530

    Basics of Objects

---
+++ w3/D3/P5.js
@@ -0,0 +1,10 @@
+// Basics of Objects
+const person ={
+    name:"Srusti",
+    age:21,
+    isStudent:false
+};
+console.log("Person",person);
+console.log("Name",person.name);
+console.log("Age",person["age"]);
+console.log("isStudent",person["isStudent"]);
