a8528758cdf45c83770f0fa95483e052ebde3345
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-04 14:07:53 +0530

    Basics of Objects

--- w3/D3/P5.js
+++ w3/D3/P5.js
@@ -4,18 +4,18 @@ const person ={
     age:21,
     isStudent:false
 };
-// console.log("Person",person);
-// console.log("Name",person.name);
-// console.log("Age",person["age"]);
+console.log("Person",person);
+console.log("Name",person.name);
+console.log("Age",person["age"]);
 
 // Add a new property
-// person.city = "Mysore";
-// console.log("Person",person);
-// // Modify
-// person.age = 31;
-// //delete
-// delete person.isStudent;
-// console.log("Person",person);
+person.city = "Mysore";
+console.log("Person",person);
+// Modify
+person.age = 31;
+//delete
+delete person.isStudent;
+console.log("Person",person);
 
 //object constructor
 const car = new Object();
