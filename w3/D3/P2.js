03b0aed1e64e2579c8562c9f188574486d2b71c4
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-04 12:19:56 +0530

    while loop

---
+++ w3/D3/P2.js
@@ -0,0 +1,13 @@
+// while loop
+console.log("while loop");
+let count = 0;
+while(count<5){
+    console.log("count:",count);
+    count ++;
+}
+// do while
+let value = 10;
+do{
+    console.log("value:",value);
+    value++;
+}while(value < 3);
