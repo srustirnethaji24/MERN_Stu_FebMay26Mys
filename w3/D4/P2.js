399e9d8d720d34c5a6627d9751b030fcca2e7b5f
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-05 10:32:46 +0530

    Array giteration

---
+++ w3/D4/P2.js
@@ -0,0 +1,14 @@
+// Array giteration
+let numArray = [1,2,3,4];
+
+for (let i =0;i<numArray.length;i++){
+    console.log(numArray[i]);
+}
+
+for(let num of numArray){
+    console.log(" ",num);
+}
+//forEach
+numArray.forEach((val,idx) => {
+     console.log("",idx,val);
+});
