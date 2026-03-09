95868b6340c6e7eabd0b737cf87910ef3699a947
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-09 15:35:10 +0530

    Event delegation

---
+++ w3/D5/P8.js
@@ -0,0 +1,22 @@
+// event delegation
+
+const taskList = document.getElementById("taskList");
+const addTask = document.getElementById("addTask");
+let taskCount = 3;
+
+taskList.addEventListener("click",function(event){
+    if(event.target.classList.contains("deleteBtn")){
+        console.log("delete button clickedc for: ",
+        event.target.parentElement.textContent.trim() 
+    );
+        event.target.parentElement.remove();
+    }
+});
+
+addTask.addEventListener("click",function(){
+    taskCount++;
+    const li = document.createElement("li");
+    li.innerHTML = "Task" + taskCount +'<button class="deleteBtn">Delete</button>';
+    taskList.appendChild(li);
+    console.log("New task created");
+});
