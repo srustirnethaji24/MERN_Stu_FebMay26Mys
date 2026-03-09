fc98fc3c4bf9513e380b5635459911c34983d590
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-09 15:34:15 +0530

    event bubbling  and stoppogation

---
+++ w3/D5/P7.js
@@ -0,0 +1,16 @@
+const outer = document.getElementById("outer");
+const inner = document.getElementById("inner");
+const innerButton = document.getElementById ("innerButton");
+
+outer.addEventListener("click",function(){
+    console.log("outer div clicked");
+});
+inner.addEventListener("click",function(){
+    console.log("inner div clicked");
+});
+
+innerButton.addEventListener("click",function(event){
+    console.log("inner Button clicked");
+    event.stopPropagation();
+    console.log("Propagation stooped at button");
+});
