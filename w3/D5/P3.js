b592268cf79446de1687ec7d672c0bebfe5ab3a1
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-09 15:24:54 +0530

    mouse events

---
+++ w3/D5/P3.js
@@ -0,0 +1,11 @@
+// mouse events
+const hoverbox = document.getElementById("hoverBox");
+
+hoverbox.addEventListener("mouseover:",function(){
+    hoverbox.textContent = "Mouse is me";
+    hoverBox.style.backgroundcolour = "lightyellow";
+});
+hoverbox.addEventListener("mouseout:",function(){
+    hoverbox.textContent = "Mouse is me";
+    hoverBox.style.backgroundcolour = "lightyellow";
+});
