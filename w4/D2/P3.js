f794e89204caea5d31a365cc92bc811026387aa0
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-11 20:32:51 +0530

    InsertAdjacentHTML js

---
+++ w4/D2/P3.js
@@ -0,0 +1,5 @@
+const container = document.getElementById("container");
+// position:beforebegin,afterbegin,beforeend,afterend 
+document.getElementById("btn").addEventListener("click",function(){
+    container.insertAdjacentHTML("beforebegin","<p>Dynamically Inserted</p>")
+});
