ddfb7ced23c7c8a43365a1cad233bc7c2ff815fa
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-11 20:12:25 +0530

    Fetch Text js

---
+++ w4/D3/P6.js
@@ -0,0 +1,18 @@
+//Introduction of Fetch API
+const introBtn = document.getElementById("introBtn");
+const output = document.getElementById("output");
+
+introBtn.addEventListener("click",function(){
+    output.textContent = "Sending request......";
+    fetch("https://jsonplaceholder.typicode.com/posts/26")
+    .then(function(response){
+        return response.text();
+    })
+    .then(function (text){
+        console.log("Text response : ",text);
+        output.textContent = text;
+    })
+    .catch(function(error){
+        output.textContent = "Unexpected fetch error: " + error.message;
+    });
+});
