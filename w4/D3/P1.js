1d1d89ba4ac8fe6fc4e443e58b9eadd651352ee0
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-11 20:06:21 +0530

    Local Storage Usage js

---
+++ w4/D3/P1.js
@@ -0,0 +1,7 @@
+const checkBtn = document.getElementById("checkBtn");
+checkBtn.addEventListener("click",function(){
+    console.log("Local storage check",typeof localStorage !== "undefined");
+    console.log("session storage check",typeof sessionStorage !== "undefined");
+    console.log("localStorage object",localStorage);
+    console.log("sessionStorage object",sessiobStorage);
+});
