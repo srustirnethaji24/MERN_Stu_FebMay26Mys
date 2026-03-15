bfd0dd64d33f09920ee6116eace9c2438aaaf3bb
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-15 13:32:42 +0530

    JS Array

---
+++ Assignment/JS Arrays/assi1.js
@@ -0,0 +1,21 @@
+let tickets = [
+    {id: "T1", priority:"high",resolved:false},
+    {id: "T2", priority:"medium",resolved:true},
+    {id: "T3", priority:"low",resolved:false},
+    {id: "T4", priority:"high",resolved:true},
+    {id: "T5", priority:"medium",resolved:false}
+];
+tickets.unshift({ id:"T1",priority: "high",resolved:false});
+tickets.push(
+    {id:"T6",priority:"medium",resolved:false},
+    {id:"T7",priority:"low",resolved:true}
+);
+let currentTicket = tickets.shift();
+let droppedticket = tickets.pop();
+let pending = tickets.filter(ticket => ticket.resolved=== false);
+let pendingIds = pending.map(ticket => ticket.id);
+ 
+console.log(currentTicket);
+console.log(droppedticket);
+console.log(pending);
+console.log(pendingIds);
