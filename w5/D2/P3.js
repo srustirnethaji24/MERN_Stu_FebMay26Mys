edc100c089835d8d984580dff9a40277a3f8e1e5
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-17 12:52:14 +0530

    Asynchronous approch of Node.js

---
+++ w5/D2/P3.js
@@ -0,0 +1,12 @@
+// Asynchronous approch of Node.js
+
+console.log("Step 1: Script started.");
+setTimeout(()=>{
+   console.log("Step 3: Delayed callback finished. F1.");
+},10000);
+
+setTimeout(function(){
+    console.log("Step 3: Delayed callback finished. F2.");
+},3000);
+
+console.log("Step 2: Script did not stop while waiting.");
