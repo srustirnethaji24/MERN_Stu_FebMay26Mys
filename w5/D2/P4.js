187bbababc1e599bd9a8e5efd0c04999fd7077d4
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-17 13:01:57 +0530

    Writting custom callback functions

---
+++ w5/D2/P4.js
@@ -0,0 +1,33 @@
+// Writting custom callback functions  
+
+function ProcessStudent(name,score,callback,monkey){
+    console.log("Student name: ",name);
+    console.log("Score: ",score);
+
+    callback(name,score);
+    monkey(name,score);
+}
+function showGrade(name,score){
+    if(score >=85){
+        console.log("A+");
+    }
+    else if(score >= 75){
+        console.log("Grade: A");
+    }
+     else if(score >= 70){
+        console.log("Grade: B");
+    }
+    else{
+        console.log("Grade: fail"); 
+    }
+}
+function showResult(name,score){
+    if(score >=70){
+        console.log(name+ " has passed.");
+    }else{
+        console.log(name+ " has failed."); 
+    }
+}
+
+ProcessStudent("loki",86,showGrade,showResult);
+// ProcessStudent("loki",85,showGrade);
