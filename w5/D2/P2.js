f9edda106c19424162048144a697a5bb6a4a6a57
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-17 12:51:57 +0530

    Basic callback variations

---
+++ w5/D2/P2.js
@@ -0,0 +1,20 @@
+//Basic callback variations
+//1.a callback with no input data
+//2.a callback that receives data from main function
+
+function runTask(callback){
+    console.log("Task is running.");
+    callback();
+}
+function runTaskWithResult(taskName,callback){
+    console.log("Processing task:",taskName);
+    callback("Task "+taskName+" finished successfully.");
+}
+function showSimpleDoneMessage(){
+    console.log("Simple callback executed.");
+}
+function showDetailedMessage(message){
+    console.log(message);
+}
+//runTask(showSimpleDoneMessage);
+runTaskWithResult("Send monthly report",showDetailedMessage);
