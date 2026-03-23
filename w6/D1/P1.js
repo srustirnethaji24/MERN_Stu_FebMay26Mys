e6921b3b4fa3748e4a40a6c11f0b5508e6423967
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-23 11:46:50 +0530

    Basics of NodeJS modules

--- w6/D1/P1.js
+++ w6/D1/P1.js
@@ -1,8 +1,8 @@
-//Basics of NodeJS modules
+//Basics of NodeJS modules 
 const moduleTitle = "NodeJs  module basics";
 function describeModule (){
     console.log("This file is its own module ")
     console.log("Title: ",moduleTitle);
     console.log("Local values inside this file unless exported");
 }
-describeModule(); // invoking the function
+describeModule();// invoking the function
