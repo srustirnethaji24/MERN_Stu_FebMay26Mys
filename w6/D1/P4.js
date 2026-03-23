5f27464bb3d34143a12e2b564c3996e7581ec365
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-23 12:31:44 +0530

    Usage of built=in and third   party modules

---
+++ w6/D1/P4.js
@@ -0,0 +1,16 @@
+// Usage of built=in and third   party modules
+
+//path is a built-in module, so it works without installation 
+const path = require("path");
+
+const invoicePath = path.join("invoices","2026","invoice_001.txt");
+console.log("Built-in module result:",invoicePath);
+
+// To use third-party package/module 
+try{
+    const _ = require("lodash");
+    console.log("Third-party module example ");
+}
+catch(error){
+    console.log("Third=party module 'lodash' is not installed.");
+}
