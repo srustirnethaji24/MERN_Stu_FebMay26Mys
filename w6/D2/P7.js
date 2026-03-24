c9eacdc61afdd409e99f66b4fe72050609c4b055
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-24 16:46:47 +0530

    SetImmediate vs SetTimeout

--- w6/D2/P7.js
+++ w6/D2/P7.js
@@ -1,12 +1,12 @@
 // setImmediate vs setTimeout
-console.log("Scheduling setTimeout and setImmediate");
+console.log("Scheduling setTimeout and setImmediate.");
 
 //callback timer
 setTimeout(() =>{
-    console.log("Timer callback grom  setTimeout");
+    console.log("Timer callback grom  setTimeout.");
 },0);
 
-//setImmediate callback
+//setImmediate callback 
 setImmediate(function(){
     console.log("setImmediate callback executed.");
 });
