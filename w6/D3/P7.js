a7211a52df2d2afab01e3b63b38392d00be13a92
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-25 12:20:35 +0530

    Introduction to buffers in NodeJS

---
+++ w6/D3/P7.js
@@ -0,0 +1,15 @@
+// Introduction to buffers in NodeJS
+// A buffer storess raw bytes
+// Here we create buffer directly from a string
+
+const textBuffer = Buffer.from("Srusti ");
+
+// The value in the buffer is the encod form the text
+console.log("Buffer object: ", textBuffer);
+console.log("Buffer length in bytes: ", textBuffer.length);
+console.log("Byte at Index 0",textBuffer[0]);
+console.log("Byte at Index 1",textBuffer[1]);
+
+//Each character is stored internallynas byte data
+// For standard ASCII letters there will be a equivalent code
+//Buffer stores numeric value between 0 to 255
