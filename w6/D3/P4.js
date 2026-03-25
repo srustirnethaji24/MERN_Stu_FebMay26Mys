8c313e447bcf13d8c7caa0ef306ec86b7770147a
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-25 10:43:01 +0530

    Reading & Writing files Aysnchronously with fs/promises

---
+++ w6/D3/P4.js
@@ -0,0 +1,22 @@
+// Reading & Writing files Aysnchronously with fs/promises
+
+const fs = require("fs/promises");
+const path =require("path");
+
+async function runPromiseBasedFileFlow() {
+    const filepath = path.join(__dirname,"promises-note.txt");
+
+    try{
+        await fs.writeFile(filepath,
+            "written using fs/promises. This work with asnc/await"
+        );
+        console.log("file written using fs/promises");
+
+        const content = await fs.readFile(filepath,"utf-8");
+        console.log(content);
+    }
+    catch(error){
+        console.log("Promise-based fs error: ",error.message);
+    }
+}
+runPromiseBasedFileFlow();
