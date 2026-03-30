275e21572729dd3401448f7cb7a35bad10b7edfb
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-30 12:20:44 +0530

    Middleware order and route specific middleware in Express.js

---
+++ w7/D1/P5.js
@@ -0,0 +1,25 @@
+// Middleware order and route specific middleware in Express.js
+
+const express = require('express');
+const app = express();
+
+// Global Middleware - runs for every request.
+app.use(function(req,res,next){
+    console.log("Global Middleware:",req.method,req.url);
+    next();
+});
+
+app.get("/users",function(req,res){
+    res.send("Home Route.");
+});
+
+app.get("/admin",function(req,res,next){
+    console.log("Route Specific Middleware For /admin");
+    next();
+},function(req,res){
+    res.send("Admin Route Reached.")
+});
+
+app.listen(4000,function(){
+    console.log("Express Server is running at http://localhost:4000");
+});
