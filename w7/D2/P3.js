30acf6a0a4cfed11a4c4c032e18521339b648964
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-31 17:18:22 +0530

    Custom-middleware

--- w7/D2/P3.js
+++ w7/D2/P3.js
@@ -1,21 +1,25 @@
-//Custom-midlleware
-
-const express = require("express");
-const app = express();
-
-function checkAdminAcess(req,res,next){
-    if (req.query.role =="admin"){
-        return res.status(403).json({success:false,message:"Admin access denied" });
-}
-next();
+//custom-middleware
+const express=require("express");
+const app =express();
+//custom middleware
+function checkAdminAccess(req,res,next){
+    if(req.query.role=="admin"){
+        return res.status(403).json({
+            success:false,
+            message:"Admin access denied"
+        });
+    }
+    next();
 }
 app.get("/",function(req,res){
-    res.send("Home route");
+    res.send("home route");
 });
-app.get("/admin",function(req,res){
-    res.json({success:true,message:"Admin route accesssed"});
+app.get("/admin",checkAdminAccess,function(req,res){
+    res.json({
+        success:true,
+        message:"Admin route accessed"
+    });
 });
-
-app.listen(4000,function(){
-    console.log("Express server running at http://localhost:4000");
+app.listen(4001,function(){
+    console.log("express server running at http://localhost:4000");
 });
