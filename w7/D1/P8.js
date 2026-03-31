650e2dd133f2fd77e973c44bd00283fe64b529fa
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-31 14:42:54 +0530

    Route parameters and query parameters

---
+++ w7/D1/P8.js
@@ -0,0 +1,15 @@
+//Route parameters and query parameters
+
+const express = require('express');
+const app = express();
+
+app.get("/products/:id",function(req,res){
+    res.json({
+        routeParameter: req.params.monkey,
+        queryParameters:req.query
+    });
+});
+
+app.listen(4000,function(){
+    console.log("Express Server is running at http://localhost:4000");
+});
