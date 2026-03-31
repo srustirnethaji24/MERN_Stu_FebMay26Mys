ca3206111f737f4c770a8fd0129060f15a246d07
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-03-31 16:04:07 +0530

    Routing:Nested routes

---
+++ w7/D2/P1.js
@@ -0,0 +1,31 @@
+// Routing
+// "/" base url
+// "/api/users"
+const express = require('express');    
+const app = express();
+
+// "/api/users" /create /delete /update /:id
+
+//Groups objects help arganize route Groups
+const apiRouter = express.Router();
+
+apiRouter.get("/users",function(req,res){
+    res.json({
+        route:"/api/users",
+        message:"Users route inside api router"
+    });
+});
+
+apiRouter.get("/orders",function(req,res){
+    res.json({
+        route:"/api/orders",
+        message:"Orders route inside api router"
+    });
+});
+
+//Mount the rouuter under the/api base path
+app.use("/api",apiRouter);
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
