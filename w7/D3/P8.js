78a9a0f6ce1d1f062541738aaa2f98d4b879a65e
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-04-02 17:31:24 +0530

    JWT flow with login, refresh-style logic and secure verification

--- w7/D3/P8.js
+++ w7/D3/P8.js
@@ -1,126 +1,138 @@
-//JWT flow with login, refresh-style login ans secure verification
-
-const jwt = require("jsonwebtoken");
+// JWT flow with login, refresh-style logic and secure verification
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
 
-const secretKey = "Mysecretekey";
-const refreshSecretKey = "MyNewsecretekey";
+const secretKey = "Mysecretkey123";
+const refreshSecretKey = "MyNewsecretkey";
 
-//in memeory storage for refresh token
+//in-memory storage for refresh token
 const refreshTokens = [];
-//authenticate access token
-function authentiacateAccessToken(req,res,next){
-    const authHeader = req.headers.authorization;
-    
-    const token = authHeader && authHeader.split(" ")[1];
-    if (!token) {
-        return res.status(401).json({
-            success: false,
-            message: "Bearer token is missing."
-        });
+
+function authenticateAccessToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Bearer token is missing.",
+    });
+  }
+  try {
+    // algorithms (verify): Only trust tokens whose alg is one of these.
+    req.user = jwt.verify(token, secretKey, {
+      algorithms: ["HS256"],
+      issuer: "jwt-example",
+    });
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        success: false,
+        message: "Access token has expired",
+      });
     }
-    try{
-        req.user = jwt.verify(token,secretKey,{
-            algorithm:["HS256"],
-            issuer:"jwt-example"
-        });
-        next();
-    }
-    catch(error){
-        if(error.name === "TokenExpiredError"){
-            return res.status(401).json({
-                success: false,
-                message: "Access token has expired."
-            });
-        }
-        return res.status(401).json({
-            success: false,
-            message: "Access token is invalid."
-        });
-    }
+    return res.status(401).json({
+      success: false,
+      message: "Access token is invalid",
+    });
+  }
 }
 
-//Post function used login
-app.post("/login",function(req,res){
-    const {email,password} = req.body;
-    if(email!=="email@email.com" || password!=="pass@123"){
-        return res.status(401).json({
-            success:false,
-            message:"Invalid Credentials"
-        });
-    }
-    const accessToken = jwt.sign({
-        userId:101,
-        email:email,
-        role:"member"
-    },secretKey,{expiresIn:"10m",algorithm:"HS256",issuer:"jwt-example"});
-
-    const refreshToken = jwt.sign({
-        userId:101,
-        email:email
-    },refreshSecretKey,
+app.post("/login", function (req, res) {
+  const { email, password } = req.body;
+  if (email !== "email@email.com" || password !== "pass@123") {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid Credentials",
+    });
+  }
+  //algorithm (sign): Use this single algorithm to sign the token.
+  const accessToken = jwt.sign(
     {
-        expiresIn:"10d",//d:days m:min h:hour
-        algorithm:"HS256",
-        issuer:"jwt-example"
-    });
-    refreshTokens.push(refreshToken);
-    res.json({
-        success:true,
-        message:"login successful",
-        accessToken:accessToken,
-        refreshToken:refreshToken
-    });
+      userId: 101,
+      email: email,
+      role: "member",
+    },
+    secretKey,
+    {
+      expiresIn: "10m",
+      algorithm: "HS256",
+      issuer: "jwt-example",
+    },
+  );
+
+  const refreshToken = jwt.sign(
+    {
+      userId: 101,
+      email: email,
+    },
+    refreshSecretKey,
+    {
+      expiresIn: "10d", //d:days m:mins h:hour
+      algorithm: "HS256",
+      issuer: "jwt-example",
+    },
+  );
+  refreshTokens.push(refreshToken);
+  res.json({
+    success: true,
+    message: "login successful",
+    accessToken: accessToken,
+    refreshToken: refreshToken,
+  });
 });
 
-app.post("/refresh",function(req,res){
-    const {refreshToken} = req.body;
-    if(!refreshToken || !refreshTokens.includes(refreshToken)){
-        return res.status(401).json({
-            success:false,
-            message:"Refresh token is missing or invalid"
-        });
-    }
-    try{
-        const decoded = jwt.verify(refreshToken,refreshSecretKey,{
-            algorithm:"HS256",
-            issuer:"jwt-example"
-        });
-        const newAccessToken = jwt.sign({
-            userId:decoded.userId,
-            email:decoded.email,
-            role:"member"
-        },secretKey,{
-            expiresIn:"10d",//d:days m:min h:hour
-            algorithm:"HS256",
-            issuer:"jwt-example"
-        });
-        res.json({
-            success:true,
-            accessToken:newAccessToken
-        });
-    }
-    catch(error){
-        res.status(403).json({
-            success:false,
-            message:"Refresh token verification failed."
-        });
-    }
-});
-app.get("/me",authentiacateAccessToken,function(req,res){
-    res.json({
-        success:true,
-        message:"Protected user route",
-        user:req.user
+app.post("/refresh", function (req, res) {
+  const { refreshToken } = req.body;
+  if (!refreshToken || !refreshTokens.includes(refreshToken)) {
+    return res.status(401).json({
+      success: false,
+      message: "Refresh token is missing or invalid",
     });
+  }
+  try {
+    const decoded = jwt.verify(refreshToken, refreshSecretKey, {
+      algorithms: "HS256",
+      issuer: "jwt-example",
+    });
+
+    const newAccessToken = jwt.sign(
+      {
+        userId: decoded.userId,
+        email: decoded.email,
+        role: "member",
+      },
+      secretKey,
+      {
+        expiresIn: "15m", //d:days m:mins h:hour
+        algorithm: "HS256",
+        issuer: "jwt-example",
+      },
+    );
+    res.json({
+      success: true,
+      accessToken: newAccessToken,
+    });
+  } catch (error) {
+    res.status(403).json({
+      success: false,
+      message: "Refresh token verification failed.",
+    });
+  }
 });
 
-app.listen(4000,function(){
-    console.log("JWT demo server running @ http://localhost:4000");
+app.get("/me", authenticateAccessToken, function (req, res) {
+  res.json({
+    success: true,
+    message: "Protected user route",
+    user: req.user,
+  });
 });
-//curl -X POST http://localhost:4000/login -H "Content-Type: application/json" -d "{\"email\":\"email@email.com\",\"password\":\"pass@123\"}"
-//curl -X POST http://localhost:4000/refresh -H "Content-Type: application/json" -d "{\"refreshToken\":\"\"}"
-//curl http://localhost:4000/me -H "Authorization:Bearer
+
+app.listen(4000, function () {
+  console.log("JWT demo server running @ http://localhost:4000");
+});
