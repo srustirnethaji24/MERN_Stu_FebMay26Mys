dbccd36c4745b9115e9aa63b252dba953616bdf2
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-04-02 15:52:39 +0530

    Secure an cookies with httpOnly and secure flags

---
+++ w7/D4/P3.js
@@ -0,0 +1,16 @@
+//  Secure an cookies with httpOnly and secure flags
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express(); 
+
+app.get("/set-session",function(req,res){const isProduction = process.env.NODE_ENV === "production";
+    res.cookie("sessioId","demo-session-123",{httpOnly:true,secure:isProduction,maxAge:30*60*1000});
+    res.send("Session cookie set eith httpOnly & environment secure flag");
+});
+
+app.get("/read-session",function(req,res){res.json({message:"Server can read the cookie value from the request",
+    sessionId:req.cookies.sessionId || "No session cookie found"});
+});
+app.listen(4000,function(){
+    console.log("JWT demo server running @ http://localhost:4000");
+});
