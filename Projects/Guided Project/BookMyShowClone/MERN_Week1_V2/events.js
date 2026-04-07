a5a830865dd6656220bf8cbafd0f9f911f63f6f5
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-04-07 18:40:21 +0530

    delete

--- Projects/Guided Project/BookMyShowClone/MERN_Week1_V2/events.js
+++
@@ -1,7 +0,0 @@
-// This file creates and exports a custom EventEmitter instance
-const EventEmitter = require ("events");
-
-// Custom EventEmitter Object
-const bookingEmitter = new EventEmitter();
-
-module.exports = bookingEmitter;
