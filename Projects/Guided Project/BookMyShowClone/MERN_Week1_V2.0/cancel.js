ae30efecdbc4ba9312c3a1ea12feeee7c5d93ab3
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-04-08 09:26:37 +0530

    To cancel the existing booking if exists

--- Projects/Guided Project/BookMyShowClone/MERN_Week1_V2.0/cancel.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V2.0/cancel.js
@@ -1,37 +1,41 @@
-// Cancel.js
-// To cancel the existing booking if exits 
+//cancel.js
+// To cancel the existing booking if exists
 const bookingEmitter = require("./events");
-const { getCurrentBooking, clearCurrentsBooking } = require("./booking");
+const {getCurrentBooking,clearCurrentBooking} = require("./booking");
+const { appendLogAsync } = require("./fileManager"); //New
 
-
-function cancelBooking(movies) {
+async function cancelBooking(movies) { //New
     const booking = getCurrentBooking();
 
     if (!booking) {
-        bookingEmitter.emit("bookingFailed", "No booking found to cancel.");
-        return null;
-    }
-    const Movie = movies.find((m) => m.id === booking.movieID);
-    if (!movie) {
-        bookingEmitter.emit("bookingFailed", "Movie data not found while cancelling booking.");
+        bookingEmitter.emit("bookingFailed","No booking found to cancel.");
         return null;
     }
 
-    const ShowTime = movie.Showtimes.find((show) => show.time.toLowerCase() === booking.time.toLowerCase());
-    if (!ShowTime) {
+    const movie = movies.find((m)=>m.id === booking.movieId);
+    if (!movie) {
+        bookingEmitter.emit("bookingFailed","Movie data not found while cancelling booking.");
+        return null;
+    }
+
+    const showtime = movie.showtimes.find((show)=>show.time.toLowerCase()===booking.time.toLowerCase());
+    if (!showtime) {
         bookingEmitter.emit("bookingFailed","Showtime data not found");
         return null;
     }
-    // restore seats
-    showTime.seatsAvailable += booking.seatCount;
 
-    //clear current Booking
-    clearCurrentsBooking();
+    //restore seats
+    showtime.seatsAvailable +=booking.seatCount;
 
+    // clear current Booking
+    clearCurrentBooking();
+
+    await appendLogAsync(`Booking cancelled: ${booking.bookingId} for ${booking.movieTitle}`); //New
     bookingEmitter.emit("bookingCancelled",booking);
 
     return booking;
 }
+
 module.exports = {
     cancelBooking
 };
