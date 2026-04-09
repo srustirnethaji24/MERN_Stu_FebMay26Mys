bb2f923c512e51889afcd78788cee23af84a16a5
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-04-09 11:27:33 +0530

    Main Express server entry point

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1.0/server.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1.0/server.js
@@ -4,5 +4,5 @@ const app = require("./app");
 const PORT = 5000;
 
 app.listen(PORT,()=>{
-    console.log(`Server running on http://localhost:$(PORT)`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
