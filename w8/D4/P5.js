6b18347e2c6104d0cf1f8f7198fd74100431bdf8
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-04-16 13:08:21 +0530

    Generate and Verify OTP

---
+++ w8/D4/P5.js
@@ -0,0 +1,42 @@
+// Generate and Verify OTP
+const crypto = require("crypto");
+
+const otpStore = {};
+
+function generateOTP(userId) {
+    const otp = crypto.randomInt(100000,999999).toString();
+    otpStore[userId] = {
+        otp,
+        expiresAt: Date.now() + 5000,
+        attempts: 0
+    };
+    console.log("OTP:",otp);
+}
+
+function verifyOTP(userId, enteredOtp) {
+    const record = otpStore[userId];
+
+    if (!record) {
+        return "No OTP";
+    }
+    if (Date.now() > record.expiresAt) {
+        return "Expired";
+    }
+    if (record.attempts >= 3){
+        return "Blocked";
+    }
+
+    record.attempts++;
+    return record.otp === String(enteredOtp)?"Valid" : "Invalid";
+}
+
+generateOTP("user1");
+
+const userEnteredOtp = otpStore["user1"].otp;
+setTimeout(() => {
+    console.log(verifyOTP("user1","132")); //Expired
+}, 10000);
+
+// console.log(verifyOTP("user1",userEnteredOtp)); //Valid
+// console.log(verifyOTP("user1","132")); //Invalid
+// console.log(verifyOTP("user2","132")); //No OTP 
