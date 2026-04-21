576849db5b91c21310ec62727af610394cf6e807
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-04-21 15:39:18 +0530

    User Schema and Model Created

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/User.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/User.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String, required: [true, "Email is required"],
         unique: true,lowercase:true,
-        match:[/^\s+@\s+\.\s+$/,"Please use a valid email"],
+        match:[/^\S+@\S+\.\S+$/,"Please use a valid email"],
         index:true,
     },
     password: {
