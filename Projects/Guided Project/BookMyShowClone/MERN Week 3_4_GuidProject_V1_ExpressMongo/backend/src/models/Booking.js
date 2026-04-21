97e315b8fda18f405b99e3a972b148c2f18db2f9
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-04-21 12:45:49 +0530

    Created boking schema model added validation ,created index and exported model

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Booking.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const bookingSchema = new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+        index:true,
+    },
+    showId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"show",
+        required:true,
+        index:true
+    },
+    seats:{
+        type:String,
+        required:true,
+    },
+    status:{
+        type:String,
+        enum:["booked","cancelled"],
+        default:"booked",
+        index:true,
+    },
+    bookingTime:{
+        type:Date,
+        default:Date.now(),
+    },
+},
+{
+    timestamps:true,
+});
+
+// Add Validation
+bookingSchema.pre("save",function (next) {
+    if (this.seats.length === 0) {
+        return next(new Error("At Least One Seat Must Be Selected"));
+    }
+    if (this.totalSeats!=this.seats.length) {
+        return next(new Error("Seat Count Mismatch"));
+    }
+    next();
+});
+
+// Compound Index
+
+bookingSchema.index({userId:1,showId:1});
+
+module.exports = mongoose.model("Booking",bookingSchema);
