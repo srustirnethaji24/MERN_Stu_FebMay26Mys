e130a02f795a6aab6d486e95d2e7af5ecf481dc6
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-04-22 13:01:23 +0530

    Created Role middleware For RBAC

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/role.middleware.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/role.middleware.js
@@ -1,7 +1,11 @@
 //  Role middleware :RBAC
 exports.authorize = (...roles)=>{
+    console.log("roles :",roles);
+    
     return(req,res,next)=>{
-        if(!req.user || !roles.includes(req,res.role)){
+        console.log("Req User : ",req.user);
+        
+        if(!req.user || !roles.includes(req.user.role)){
               return res.status(403).json({
                 success: false,
                 message:"Access denied: insufficient permission",
