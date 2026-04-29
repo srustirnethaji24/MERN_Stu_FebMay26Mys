1f075ca44d763af689f4ae64eea9ddc34c2d8f6e
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-04-29 14:01:17 +0530

    TTL : Time to Live

---
+++ w8/D4/P3.js
@@ -0,0 +1,30 @@
+// TTL : Time to Live
+const mongoose = require('mongoose');
+async function main() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/datedb');
+        console.log("MongoDB connected");
+
+        const otpSchema = new mongoose.Schema({
+            code: String,
+            createdAt:{
+                type: Date,
+                default:Date.now,
+                expires: 30 //30 seconds
+            }
+        });
+
+        const OTP = mongoose.model("OTP",otpSchema);
+        await OTP.deleteMany();
+
+        await OTP.create({code:"999999"});
+        console.log("OTP Created.")
+    } catch (error) {
+         console.error("Error:", error.message);
+    }
+     finally {
+            await mongoose.disconnect();
+            console.log("Db disconnected");
+        }
+}
+main();
