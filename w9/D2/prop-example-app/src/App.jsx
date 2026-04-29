56e5dee5bbec98220a44910c1ef9ea80c4c7b8d1
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-04-29 17:16:09 +0530

    Higher order component(HOC) example

---
+++ w9/D2/prop-example-app/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { PropBasics } from "./components/P1";
+import { PropDestructuring } from "./components/P2";
+import { PropsChildren } from "./components/P3";
+import { PropDrillingDemo } from "./components/P4";
+import { SharedStateParent } from "./components/P5";
+import { CompositionDemo } from "./components/P6";
+import { ObjectsAndArrayProps } from "./components/P7";
+import { PropsImmutability } from "./components/P8";
+import { PropsState } from "./components/P9";
+import { FunctionProps } from "./components/P10";
+import { PropTypesDemo } from "./components/P11";
+import { HOCExample } from "./components/P12";
+
+function App() {
+  return (
+    <>
+      {/* <PropBasics /> */}
+      {/* <PropDestructuring /> */}
+      {/* <PropsChildren /> */}
+      {/* <PropDrillingDemo /> */}
+      {/* <SharedStateParent/> */}
+      {/* <CompositionDemo /> */}
+      {/* <ObjectsAndArrayProps /> */}
+      {/* <PropsImmutability /> */}
+      {/* <PropsState/> */}
+      {/* <FunctionProps /> */}
+      {/* <PropTypesDemo /> */}
+      <HOCExample />
+    </>
+  )
+}
+export default App
