7297f9687025e49610e8a2370e1160ab0813515d
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-04-29 10:31:38 +0530

    Created booking & cancelBooking functions and exported it

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/services/booking.service.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/services/booking.service.js
@@ -2,38 +2,37 @@ const Booking = require("../models/Booking");
 const Show = require("../models/Show");
 
 // Create Booking
-exports.createBooking = async (userId, { showId, seats }) => {
-    // Get show 
+exports.createBooking = async (userId,{showId,seats}) => {
+    // 1. Get show
     const show = await Show.findById(showId);
 
     if (!show) {
         throw new Error("Show not found");
     }
 
-    //  2. Validate seats
-    const SelectedSeats = show.seats.filter((seat) =>
-        seats.includes(seat.seatNumber)
-    );
-    if (SelectedSeats.length != seats.length) {
-        throw new Error("seats do not exist");
+    // 2. validate seats
+    const selectedSeats = show.seats.filter((seat)=>
+        seats.includes(seat.seatNumber));
+    if (selectedSeats.length!=seats.length) {
+       throw new Error("seats do not exist");        
     }
 
-    //  3. Check if already book
-    for (let seat of SelectedSeats) {
+    // 3. Check if already booked
+    for(let seat of selectedSeats){
         if (seat.isBooked) {
             throw new Error(`Seat ${seat.seatNumber} is already booked`);
         }
     }
 
     // 4. Mark seats as booked
-    show.seats = show.seats.map((seat) => {
+    show.seats = show.seats.map((seat)=>{
         if (seats.includes(seat.seatNumber)) {
             seat.isBooked = true;
         }
         return seat;
     });
-    //  5 .Update available seats
-    show.availableSeats -= seats.length;
+    // 5. Update available seats
+    show.availableSeats -=seats.length;
 
     await show.save();
 
@@ -47,68 +46,69 @@ exports.createBooking = async (userId, { showId, seats }) => {
 // Get user bookings
 exports.getUserBookings = async (userId) => {
     const bookings = await Booking.find({
-        userId, status: "booked",
+        userId,
+        status:"booked",
     })
-        .populate({
-            path: "showId",
-            select: "date time availableSeats movieId",
-            populate: {
-                path: "movieId",
-                select: "title genre",
-            },
-        })
-        .sort("-createdAt");
-
-    // Transform response
-    return bookings.map((bookings) => ({
-        bookingId: booking._id,
-        seats: booking.seats,
-        totalSeats: booking.totalSeats,
-        status: booking.status,
-        bookingTime: booking.bookingTime,
-
-        show: {
-            id: booking.showId._id,
-            date: booking.showId.date,
-            time: booking.showId.time,
-            availableSeats: booking.showId.availableSeats,
+    .populate({
+        path:"showId",
+        select:"date time availableSeats movieId",
+        populate:{
+            path: "movieId",
+            select:"title genre",
         },
-        movie: {
-            id: booking.showId.movieId._id,
-            title: booking.showId.movieId.title,
-            genre: booking.showId.movieId.genre,
+    })
+    .sort("-createdAt");
+
+    //Transform response
+    return bookings.map((booking)=>({
+        bookingId:booking._id,
+        seats:booking.seats,
+        totalSeats:booking.totalSeats,
+        status:booking.status,
+        bookingTime:booking.bookingTime,
+
+        show:{
+            id:booking.showId._id,
+            date:booking.showId.date,
+            time:booking.showId.time,
+            availableSeats:booking.showId.availableSeats,
+        },
+        movie:{
+            id:booking.showId.movieId._id,
+            title:booking.showId.movieId.title,
+            genre:booking.showId.movieId.genre,
         },
     }));
 };
 
 // Cancel booking
 exports.cancelBooking = async (bookingId,userId) => {
-    const booking = await booking.findById(bookingId);
+    const booking = await Booking.findById(bookingId);
     if(!booking)
-        throw new error("Booking not found");
+        throw new Error("Booking not found");
 
     if (booking.userId.toString()!==userId.toString()) {
-        throw new Error("Unauthorized"); 
+        throw new Error("Unauthorized");
     }
 
     if(booking.status === "cancelled"){
         throw new Error("Already cancelled.");
     }
 
-    //  1. Get show
+    // 1.get show
     const show = await Show.findById(booking.showId);
-    //  2.Release seats
-    show.seats = show.seats.map((seat) =>{
-        if(booking.seats.includes(seat.seatNumber)){
+    // 2. Release seats
+    show.seats = show.seats.map((seat)=>{
+        if (booking.seats.includes(seat.seatNumber)) {
             seat.isBooked = false;
         }
         return seat;
     });
-    //  3.UPdate available seats
+    // 3. Update available seats
     show.availableSeats +=booking.seats.length;
     await show.save();
 
-    //  4. Update boking
+    // 4. Update booking
     booking.status = "cancelled";
-    await booking.save(); 
+    await booking.save();    
 };
