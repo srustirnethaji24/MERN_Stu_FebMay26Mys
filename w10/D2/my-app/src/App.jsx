5f3b347b58e20b177bee5223828528c1017c55f4
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-05-06 12:58:04 +0530

    useRef hook

--- w10/D2/my-app/src/App.jsx
+++ w10/D2/my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { UseContextIntro } from "./components/P1";
 import { CustomHooksIntro} from "./components/P2";
+import{ UseRefIntro } from "./components/P3";
 function App() {
   return (
     <>
-      {/* <UseContextIntro />        */}
-      <CustomHooksIntro />
+      {/* <UseContextIntro /> */}
+      {/* <CustomHooksIntro /> */}
+      <UseRefIntro />
     </>
   )
 }
---
+++ w10/D2/my-app/src/components/P3.jsx
@@ -0,0 +1,55 @@
+// useRef hook
+// useRef cretates a mutable object
+// The object has one property: current
+// It persists across renders
+
+import { useRef, useState, useEffect } from "react";
+
+// Why do we use it?
+// Direct DOM access
+// Persist values without triggering re-renders
+
+export function UseRefIntro() {
+    // 1 Reference : DOM : This  will point to the input element
+
+    const inputRef = useRef(null);
+
+    // 2 Value reference : This stores the previous count value. Changing it does not re-render the UI by itself
+
+    const previousCountRef = useRef(0);
+
+    const [count, setCount] = useState(0);
+    const [text, setText] = useState('');
+
+    useEffect(() => {
+        inputRef.current.focus();
+    },[]);
+
+    useEffect(() => {
+        previousCountRef.current = count;
+    },[count]);
+
+    const handleFocusInput = () => {
+        inputRef.current.focus();
+    };
+
+    const handleIncrement = () => {
+        setCount((prev) => prev +1);
+    };
+
+    return (
+        <section>
+            <div>
+                <h3>1. Focus input using useRef</h3>
+                <input type="text" value={text} ref={inputRef} onChange={(e) => setText(e.target.value)} placeholder="Enter  something"/>
+                <button onClick={handleFocusInput}>Focus Input</button>
+            </div>
+            <div>
+                <h3>2.Store previous value using UseRef</h3>
+                <p> Current count:{count}</p>
+                <p> Previous count:{previousCountRef.current}</p>
+                <button onClick={handleIncrement}>Increment Count</button>
+            </div>
+        </section>
+    )
+}
