9908d832ea4ffa9407390059d81892caf923b296
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-05-07 13:50:36 +0530

    Event Delegation in React

--- w10/D3/my-event-app/src/App.jsx
+++ w10/D3/my-event-app/src/App.jsx
@@ -1,17 +1,22 @@
+import { useState } from 'react'
+import { EventBasics } from './components/P1'
+import { EventObject } from './components/P2'
+import { PassingArguments } from './components/P3'
+import { UseCallbackEvents } from './components/P5'
+import { EventDelegation } from './components/P6'
 import './App.css'
-import { EventBasics } from "./components/P1";
-import { EventObject } from "./components/P2";
-import { PassingArguments } from './components/P3';
-import { UseCallbackEvents } from './components/P5';
 
 function App() {
-   return (
+
+  return (
     <>
-      {/* <EventBasics /> */}
-      {/* <EventObject /> */}
-      {/* {PassingArguments} */}
-      <UseCallbackEvents />
+      {/* < EventBasics /> */}
+      {/* < EventObject /> */}
+      {/* < PassingArguments /> */}
+      {/* < UseCallbackEvents /> */}
+      < EventDelegation />
     </>
   )
 }
+
 export default App
