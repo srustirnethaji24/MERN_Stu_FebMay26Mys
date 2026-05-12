9ad56e4814f46a0d9db61226a01be15fdf90a73a
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-05-12 14:50:14 +0530

    Nested Roters react

---
+++ w11/D2/my-app/src/components/P1.jsx
@@ -0,0 +1,22 @@
+// Nested Routes - a route inside another route
+// Dashboard - Profile / settings / reports
+
+import {Link, Outlet} from "react-router-dom";
+export function NestedRoutes() {
+    return (
+        <div>
+            <h2>Nested Routes</h2>
+            <nav>
+                <Link to = "/dashboard">Dashboard</Link> | {''};
+                <Link to = "/dashboard/profile">Profile</Link> | {""};
+                <Link to = "/dashboard/settings">Settings</Link> | {"  "};
+
+                <hr />
+                { /* <Outlet /> is the place where matched child routes will render */ }
+                { /* Without <Outlet />, child routes will not appear inside the parent Layout */ }
+
+                < Outlet />
+            </nav>
+        </div>
+    );
+}
---
+++ w11/D2/my-app/src/components/P3.jsx
@@ -0,0 +1,13 @@
+// Multiple Dynamic Parameters
+import { useParams } from "react-router-dom";
+
+export function MultipleDynamicParams() {
+    const { userId, orderId } = useParams();
+    return (
+        <div>
+            <h2>Multiple Dynamic Params</h2>
+            <p>User ID from URL: { userId }</p>
+            <p>OrderId from URL: { orderId }</p>
+        </div>
+    );
+}
