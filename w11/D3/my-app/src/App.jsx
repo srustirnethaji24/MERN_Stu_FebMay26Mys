e6137fd937c165d9747daaa15a37bf7b6367845f
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-05-14 10:38:11 +0530

    Pagination and Caching

--- w11/D3/my-app/src/App.jsx
+++ w11/D3/my-app/src/App.jsx
@@ -2,15 +2,17 @@ import {ManagingApiState} from "./components/P1";
 import {UseEffectLifecycle} from "./components/P2"
 import {AxiosLifecycle} from "./components/P3"
 import {CURDOperation} from "./components/P4"
+import {PaginationCaching} from "./components/P5"
 import './App.css'
 
 function App() {
   return (
     <>
-        {/* <ManagingApiState /> */}
-       {/* {<UseEffectLifecycle />} */}
+       {/* <ManagingApiState />
+       <UseEffectLifecycle /> */}
        {/* <AxiosLifecycle /> */}
-       <CURDOperation />
+       {/* <CURDOperation /> */}
+       <PaginationCaching />
     </>
   )
 }
