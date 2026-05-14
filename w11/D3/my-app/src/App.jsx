12fb39094014b2479a0b670d1c1570e59a5343ed
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-05-14 09:46:12 +0530

    New react app created

---
+++ w11/D3/my-app/src/App.jsx
@@ -0,0 +1,18 @@
+import {ManagingApiState} from "./components/P1";
+import {UseEffectLifecycle} from "./components/P2"
+import {AxiosLifecycle} from "./components/P3"
+import {CURDOperation} from "./components/P4"
+import './App.css'
+
+function App() {
+  return (
+    <>
+        {/* <ManagingApiState /> */}
+       {/* {<UseEffectLifecycle />} */}
+       {/* <AxiosLifecycle /> */}
+       <CURDOperation />
+    </>
+  )
+}
+
+export default App
